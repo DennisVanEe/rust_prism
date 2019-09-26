efed1534dc00442f8678c45cb434e40c856115d6
Dennis van Ee <dennis@vanee.us>
2019-09-26 07:28:02 -0700

    work on sampler code

--- src/sampler/mod.rs
+++ src/sampler/mod.rs
@@ -3,10 +3,24 @@ use crate::camera::CameraSample;
 
 const ONE_MINUS_EPS: f64 = 0.99999999999999989;
 
+// Each thread, when working on a tile, gets access to a single Sampler.
+
 pub trait Sampler {
     fn start_pixel(&mut self, pixel: Vec2<usize>);
+
+    // This gets called constantly until it returns false.
+    // When it does it means we have sampled the pixel sample number of times.
+    fn start_next_sample(&mut self) -> bool;
+
+    fn get_num_pixel_samples(&self) -> u64;
     
     // Returns values for samples:
+
+    fn request_1d_array(&mut self, size: usize);
+    fn request_2d_array(&mut self, size: usize);
+
+    fn get_1d_array(&mut self, size: usize) -> &[f64];
+    fn get_2d_array(&mut self, size: usize) -> &[Vec2<f64>];
 
     fn get_1d(&mut self) -> f64;
     fn get_2d(&mut self) -> Vec2<f64>;
---
+++ src/sampler/stratified.rs
@@ -0,0 +1,7 @@
+use crate::sampler::Sampler;
+
+pub struct Stratified {
+    // The number of pixel samples we will use per pixel
+    num_pixel_samples: u64;
+    
+}
